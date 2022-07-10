70b5aa0f8c212618bed69277bbe40d49f3264d1e
sagegod <cdeng1999@gmail.com>
2022-07-10 16:52:34 -0700

    added class selection as well as functionality behind class choice

--- public/js/stats.js
+++ public/js/stats.js
@@ -1,6 +1,7 @@
 const { CharClass } = require("../../models")
 
 const statBtn = document.getElementById("dice");
+const confirmBtn = document.getElementById("confirm");
 
 function createStats () {
   
@@ -16,26 +17,55 @@ function createStats () {
   let luk = document.getElementById("luk");
   
   health.innerHTML = "Health: "
-  healthStat = document.createTextNode(getStr + getDex)
-  health.appendChild(healthStat);
-  
+  if (charClass.value == "Warrior") {
+    healthStat = document.createTextNode(getStr * 2 + getDex)
+    health.appendChild(healthStat);
+    } else if (charClass.value == "Hunter") {
+      healthStat = document.createTextNode(getStr + getDex *2)
+      health.appendChild(healthStat);
+    } else {
+    healthStat = document.createTextNode(getStr+ getDex)
+    health.appendChild(healthStat);
+    }
+ 
   str.innerHTML = "Str: "
+  if (charClass.value == "Warrior") {
+  strStat = document.createTextNode(getStr * 2)
+  str.appendChild(strStat);
+  } else {
   strStat = document.createTextNode(getStr)
   str.appendChild(strStat);
-  
+  }
+
   dex.innerHTML = "Dex: "
+  if (charClass.value == "Hunter") {
+    dexStat = document.createTextNode(getDex*2)
+    dex.appendChild(dexStat);
+  } else {
   dexStat = document.createTextNode(getDex)
   dex.appendChild(dexStat);
+  }
   
   int.innerHTML = "Int: "
-  intStat = document.createTextNode(getInt)
+  if (charClass.value == "Mage") {
+  intStat = document.createTextNode(getInt*2)
   int.appendChild(intStat);
- 
-  luk.innerHTML = "Luk: "
-  lukStat = document.createTextNode(getLuk)
-  luk.appendChild(lukStat);
+  } else {
+    intStat = document.createTextNode(getInt)
+    int.appendChild(intStat);
+  }
   
+
+  luk.innerHTML = "Luk: "
+  if (charClass.value == "Thief") {
+  lukStat = document.createTextNode(getLuk*2)
+  luk.appendChild(lukStat);
+  } else {
+    lukStat = document.createTextNode(getLuk)
+    luk.appendChild(lukStat);
+  }
   document.getElementById("confirm").className ="btn btn-primary";
+
 
 };
 
@@ -43,12 +73,12 @@ function createStats () {
 
 statBtn.addEventListener("click", createStats);
 
+confirmBtn.addEventListener("click", finalChoice);
+
 
 const charClass = document.getElementById("charClass");
 
-if (charClass == "Warrior") {
-  getStr * 2
+function finalChoice() {
+
 }
-
-
 
