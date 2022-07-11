0d30824ed74b11076f62dca5278d868ec49329c7
zzcipod <jonzzheng@gmail.com>
2022-07-11 12:21:19 -0700

    changebybryon

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const charRoutes = require('./charRoutes');
-
+const projectRoutes = require('./projectRoutes');
 
 router.use('/chars', charRoutes);
-
+router.use('/projects', projectRoutes);
 
 module.exports = router;
