279bf25217a65691499643d3a5e5446f95e25a00
zzcipod <jonzzheng@gmail.com>
2022-07-12 22:50:22 -0700

    remove U

--- public/js/shop.js
+++ public/js/shop.js
@@ -1,5 +1,5 @@
 function changeBackgroundImage() {
-    document.body.style.backgroundImage = "url('/images_character/intro_page.jpg')";
+    document.body.style.backgroundImage = "url('/images_character/shop.jpg')";
     document.body.style.backgroundRepeat = "no-repeat";
     document.body.style.backgroundSize = "cover";
   }
