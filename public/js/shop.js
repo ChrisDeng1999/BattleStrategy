cc203c9b51550becf9d25268b5386d4ac799b653
zzcipod <jonzzheng@gmail.com>
2022-07-12 22:22:11 -0700

    store

---
+++ public/js/shop.js
@@ -0,0 +1,7 @@
+function changeBackgroundImage() {
+    document.body.style.backgroundImage = "url('/images_character/intro_page.jpg')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "cover";
+  }
+  
+  changeBackgroundImage ();
