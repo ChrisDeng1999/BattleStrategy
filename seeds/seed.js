fb32ae41ecb9c61405aa0b1296b9ab17ad52d855
sagegod <cdeng1999@gmail.com>
2022-07-14 09:14:49 -0700

    added seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,23 +1,16 @@
 const sequelize = require('../config/connection');
-const { User, Monster } = require('../models');
+const { User } = require('../models');
 
-const monsterData = require('./monsterData.json');
+const userData = require('./userData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const monsters = await User.Create(monsterData, {
+  await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
-  //trying to create monsters
- 
-  for (const monster of monsterData) {
-    await Monster.create({
-      ...monster,
-      monster_id: monsters[Math.floor(Math.random() * monsters.length)].id,
-    });
-  }
+
 
   process.exit(0);
 };
